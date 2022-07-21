31037b832a9367f58c052f159f6decfd5d577aed
katherinegreen <rendong0809@gmail.com>
2022-07-21 14:30:27 +1000

    change to v6

--- src/components/quotes/QuoteForm.js
+++ src/components/quotes/QuoteForm.js
@@ -1,13 +1,10 @@
-import { Fragment, useRef, useState } from 'react';
-import { Prompt } from 'react-router-dom';
+import { Fragment, useRef, useState } from "react";
 
-import Card from '../UI/Card';
-import LoadingSpinner from '../UI/LoadingSpinner';
-import classes from './QuoteForm.module.css';
+import Card from "../UI/Card";
+import LoadingSpinner from "../UI/LoadingSpinner";
+import classes from "./QuoteForm.module.css";
 
 const QuoteForm = (props) => {
-  const [isEntering, setIsEntering] = useState(false);
-
   const authorInputRef = useRef();
   const textInputRef = useRef();
 
@@ -22,28 +19,10 @@ const QuoteForm = (props) => {
     props.onAddQuote({ author: enteredAuthor, text: enteredText });
   }
 
-  const finishEnteringHandler = () => {
-    setIsEntering(false);
-  };
-
-  const formFocusedHandler = () => {
-    setIsEntering(true);
-  };
-
   return (
     <Fragment>
-      <Prompt
-        when={isEntering}
-        message={(location) =>
-          'Are you sure you want to leave? All your entered data will be lost!'
-        }
-      />
       <Card>
-        <form
-          onFocus={formFocusedHandler}
-          className={classes.form}
-          onSubmit={submitFormHandler}
-        >
+        <form className={classes.form} onSubmit={submitFormHandler}>
           {props.isLoading && (
             <div className={classes.loading}>
               <LoadingSpinner />
@@ -51,15 +30,15 @@ const QuoteForm = (props) => {
           )}
 
           <div className={classes.control}>
-            <label htmlFor='author'>Author</label>
-            <input type='text' id='author' ref={authorInputRef} />
+            <label htmlFor="author">Author</label>
+            <input type="text" id="author" ref={authorInputRef} />
           </div>
           <div className={classes.control}>
-            <label htmlFor='text'>Text</label>
-            <textarea id='text' rows='5' ref={textInputRef}></textarea>
+            <label htmlFor="text">Text</label>
+            <textarea id="text" rows="5" ref={textInputRef}></textarea>
           </div>
           <div className={classes.actions}>
-            <button onClick={finishEnteringHandler} className='btn'>Add Quote</button>
+            <button className="btn">Add Quote</button>
           </div>
         </form>
       </Card>
